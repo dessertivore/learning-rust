c6b37bb0b8a0d98921931640bc212f55eeb177ee
Sophie L <slandau@forestreet.com>
2024-12-23 14:16:23 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Repo for learning Rust and playing around with it
---
+++ hello_world.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// Just learning to run my first lines of Rust
+// To run the code, use the command below:
+// rustc hello_world.rs
+// ./hello_world
