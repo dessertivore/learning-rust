acf563a27105fcba3a7049bab0a713769275e82a
Sophie L <slandau@forestreet.com>
2024-12-24 10:07:24 +0000

    Async addition endpoint

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 `src` contains a test API with some basic endpoints.
 Current endpoints are:
 `/colour` - `GET`
+`/addition` - `POST`
 
 ## To run the API
 ```sh
@@ -10,3 +11,8 @@ cargo run
 curl localhost:3000/colour
 ```
 This will return e.g. `The best colour is blue!` - a random colour is picked each time.
+
+```sh
+curl -X POST localhost:3000/addition -H "Content-Type: application/json" -d '{"nums":"1,2,3"}'
+```
+This will return `The sum of the numbers is 6` after 2 seconds.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use axum::{extract::Json, routing::get, routing::post, Router};
 use rand::Rng;
 use serde::Deserialize;
+use tokio::{task::{self, JoinHandle}, time::{sleep,Duration}};
 use std::net::SocketAddr;
-use log::info;
 
+/// This function picks a random colour from a list of colours and returns it as a string.
 async fn pick_random_colour() -> String {
     let colours = [
         "red", "green", "blue", "yellow", "purple", "orange", "pink", "brown", "black", "white",
@@ -19,15 +20,37 @@ struct NumList {
     nums: String,
 }
 
+/// This function takes a list of numbers as a JSON payload and returns the sum of 
+/// the numbers, but first, it spawns 20 async tasks that each sleep for 2 seconds.
+/// This is just an exercise to demonstrate how async tasks work in Rust - the 
+/// 'sleep' tasks will run in parallel and the total time taken to complete the
+/// function will be around 2 seconds.
 async fn add_nums(Json(payload): Json<NumList>) -> String {
+    let mut handles: Vec<JoinHandle<()>> = vec![];
+    let now = tokio::time::Instant::now();
+    for i in 0..20 {
+        let handle = task::spawn(async move {
+            sleep(Duration::from_secs(2)).await;
+            println!("2 seconds have now passed {} times", i);
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
     let sum: i32 = payload.nums
         .split(',')
         .map(|s| s.trim().parse::<i32>().unwrap_or(0))
         .sum();
     println!("The sum of the numbers is {}", sum);
+    let elapsed = now.elapsed();
+    println!("Time elapsed: {:?}", elapsed);
     return format!("The sum of the numbers is {}", sum);
 }
 
+/// This function initializes our router with two routes: one for the colour endpoint
+/// and one for the addition endpoint.
 fn init_router() -> Router {
     return 
     Router::new().
@@ -35,6 +58,7 @@ fn init_router() -> Router {
     .route("/addition", post(add_nums));
 }
 
+/// This is the main function that starts our server.
 #[tokio::main]
 async fn main() {
     // build our application with a single route
