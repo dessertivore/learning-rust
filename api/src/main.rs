82095fd812df1ab5e49176465c8770dabcad6ee4
Sophie L <slandau@forestreet.com>
2024-12-31 15:13:30 +0000

    Start trying to get thread-unsafe endpoint

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,10 @@
-use axum::{extract::Json, routing::get, routing::post, Router};
+use axum::{ extract::Json, routing::{get, post}, Extension, Router};
 use rand::Rng;
-use serde::Deserialize;
-use tokio::{task::{self, JoinHandle}, time::{sleep,Duration}};
-use std::net::SocketAddr;
+use serde::{Serialize,Deserialize};
+use tokio::{task::{self, JoinHandle}, time::{sleep,Duration}, sync::Mutex};
+use std::{cell::{Cell, UnsafeCell}, clone, marker::PhantomData, net::SocketAddr, sync::{atomic::AtomicUsize, Arc}};
+use axum_macros::debug_handler;
+
 
 /// This function picks a random colour from a list of colours and returns it as a string.
 async fn pick_random_colour() -> String {
@@ -18,6 +20,21 @@ async fn pick_random_colour() -> String {
 #[derive(Deserialize)]
 struct NumList {
     nums: String,
+}
+
+#[derive(Copy, Clone, Debug, Default)]
+struct NotSyncMarker(PhantomData<Cell<*mut usize>>);
+
+#[derive(Serialize, Deserialize, clone::Clone)]
+struct Counter {
+    count: usize,
+}
+
+#[derive(clone::Clone)]
+
+struct UnsafeCounter {
+    counter: Cell<usize>,
+    _marker: NotSyncMarker,
 }
 
 /// This function takes a list of numbers as a JSON payload and returns the sum of 
@@ -49,19 +66,31 @@ async fn add_nums(Json(payload): Json<NumList>) -> String {
     return format!("The sum of the numbers is {}", sum);
 }
 
+// async fn increment_counter(Extension(counter): Extension<Arc<Mutex<Counter>>>) -> Json<Counter> {
+async fn increment_counter(counter: Extension<UnsafeCounter>) -> Json<Extension<UnsafeCounter>> {
+    let counter_ref = counter.counter.get() +1;
+    counter.counter.set(counter_ref);
+    return Json(counter);
+    
+}
+
 /// This function initializes our router with two routes: one for the colour endpoint
 /// and one for the addition endpoint.
 fn init_router() -> Router {
+    let counter = UnsafeCounter{counter: Cell::new(0), _marker: NotSyncMarker(PhantomData)};
     return 
-    Router::new().
-    route("/colour", get(|| pick_random_colour()))
+    Router::new()
+    .route("/colour", get(|| pick_random_colour()))
+    .route("/counter", get(|| increment_counter).layer(Extension(counter)))
     .route("/addition", post(add_nums));
 }
 
 /// This is the main function that starts our server.
 #[tokio::main]
+#[debug_handler]
 async fn main() {
-    // build our application with a single route
+    
+    // build our application with a Router with multiple routes
     let app: Router = init_router();
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
     axum_server::bind(addr)
--- client/src/main.rs
+++ client/src/main.rs
@@ -9,7 +9,7 @@ struct NumList {
 
 /// This function makes a post request to the server with a payload of numbers, and returns
 /// the response as a string.
-async fn call_addition_endpoint(payload: NumList) -> String {
+async fn send_payload(payload: NumList) -> String {
     let client = reqwest::Client::new();
     let resp = client
         .post("http://localhost:8000/addition")
@@ -20,15 +20,7 @@ async fn call_addition_endpoint(payload: NumList) -> String {
     return resp.text().await.unwrap();
 }
 
-/// This is the main function that makes a get request, and then 20 post requests to 
-/// the server in order to test async functionality.
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Call colour endpoint and print response to console
-    let resp = reqwest::get("http://localhost:8000/colour").await?;
-    let body = resp.text().await?;
-    println!("Response: {}", body);
-
+async fn call_addition_endpoint() {
     let body_for_post = NumList {
         nums: "1,2,3,4".to_string(),
     };
@@ -43,7 +35,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for i in 0..20 {
         let body_for_post_clone = body_for_post.clone();
         let handle = task::spawn(async move {
-            let sum = call_addition_endpoint(body_for_post_clone).await;
+            let sum = send_payload(body_for_post_clone).await;
             println!("Response from /addition: {}. Endpoint has bee called {} times.", sum,i);
         });
         handles.push(handle);
@@ -52,9 +44,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for handle in handles {
         handle.await.unwrap();
     }
-
-
     println!("Time elapsed: {:?}", now.elapsed());
+
+}
+
+async fn call_counter() -> String {
+    let resp = reqwest::get("http://localhost:8000/counter").await;
+    let body = resp.unwrap().text().await.unwrap();
+    return body;
+}
+/// This is the main function that makes a get request, and then 20 post requests to 
+/// the server in order to test async functionality.
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Call colour endpoint and print response to console
+    let resp = reqwest::get("http://localhost:8000/colour").await?;
+    let body = resp.text().await?;
+    println!("Response: {}", body);
+
+    // Call addition endpoint and print response to console
+    // call_addition_endpoint().await;
+    let mut handles = vec![];
+    for i in 0..1000 {
+        let handle = task::spawn(async move {
+            let body = call_counter().await;
+            println!("Response from /counter: {}. Endpoint has been called {} times.", body, i);
+        });
+        handles.push(handle);
+    }
+    // Await all the spawned tasks
+    for handle in handles {
+        handle.await.unwrap();
+    }
+    
 
     Ok(())
 }
