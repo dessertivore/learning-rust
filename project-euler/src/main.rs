70a327d1d7fdb8a63e74cca4541f72aa45c35a1f
Sophie L <slandau@forestreet.com>
2025-02-10 21:47:23 +0000

    Project euler problem 3

--- project-euler/src/main.rs
+++ project-euler/src/main.rs
@@ -12,42 +12,69 @@ fn problem_1() {
     println!("The sum of all multiples of 3 or 5 below 1000 is: {}", sum);
 }
 
-
-fn fib(term: i64, cache: &mut LruCache<i64,i64>) -> i64{
+fn fib(term: i64, cache: &mut LruCache<i64, i64>) -> i64 {
     if term == 0 {
         return 0;
     }
     if term == 1 {
-        return 1; 
+        return 1;
     }
-    if term ==2{
+    if term == 2 {
         return 2;
     } else {
-    if let Some(&result) = cache.get(&term) {
-        return result;
+        if let Some(&result) = cache.get(&term) {
+            return result;
+        }
+        let result = fib(term - 1, cache) + fib(term - 2, cache);
+        cache.put(term, result);
+        result
     }
-    let result = fib(term - 1, cache) + fib(term - 2, cache);
-    cache.put(term, result);
-    result
-}}
+}
 
-fn problem_2(term: i64, cache: &mut LruCache<i64,i64>) -> i64{
+fn problem_2(term: i64, cache: &mut LruCache<i64, i64>) -> i64 {
     let mut sum = 0;
     let mut fib_num = 0;
     let mut idx = 0;
-    while fib_num < term{
-        fib_num=fib(idx,cache);
-        if fib_num %2==0{
+    while fib_num < term {
+        fib_num = fib(idx, cache);
+        if fib_num % 2 == 0 {
             sum += fib_num;
         };
-        idx+=1;
+        idx += 1;
     }
-    return sum}
+    return sum;
+}
 
+fn problem_3(mut term: i64) -> i64 {
+    let mut prev_primes = vec![1];
+    let mut current = 3;
+    let mut max_prime = 3;
+    let root = (term as f64).sqrt();
+    let root_as_int = root.round() as i64;
 
+    while term % 2 == 0 {
+        // divide by 2 until it is an odd number
+        term = term / 2;
+    }
+
+    while current <= root_as_int + 1 {
+        if term % current == 0 {
+            // If current num is a factor, divide term by it until no longer a factor
+            while term % current == 0 {
+                term = term / current;
+            }
+            max_prime = current;
+            prev_primes.push(current);
+        }
+        // skip even numbers as our term is an odd number
+        current += 2
+    }
+    return max_prime;
+}
 
 fn main() {
     problem_1();
-    let mut cache: LruCache<i64,i64> = LruCache::new(NonZeroUsize::new(2).unwrap());
-    println!("{}",problem_2(4000000, &mut cache))
-}   
+    let mut cache: LruCache<i64, i64> = LruCache::new(NonZeroUsize::new(2).unwrap());
+    println!("{}", problem_2(4000000, &mut cache));
+    println!("{}", problem_3(600851475143));
+}
